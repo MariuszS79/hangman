7509753548b23eaad979e7454476697e409199d0
MariuszS79 <54702756+MariuszS79@users.noreply.github.com>
2020-12-08 17:16:29 +0000

    081220

--- hangman.py
+++ hangman.py
@@ -1,3 +1,5 @@
+import random
+
 word_list=["empire", "catapult", "vehicle"]
 
 hangman_stages=['''
@@ -52,4 +54,24 @@ hangman_stages=['''
 =========''']
 
 
-print(hangman_stages[6])
+
+
+
+print("Hello and welcome to the Hangman game")
+
+random_word=random.choice(word_list)
+
+word_to_guess=[]
+print(random_word)
+for i in range(len(random_word)):
+    word_to_guess.append("_")
+
+letter=input("Please quess letter: ")
+
+if letter in random_word:
+    print("yes")
+    splitted=list(random_word)
+    print(splitted)
+
+
+
